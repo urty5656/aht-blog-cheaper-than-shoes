e4790e9f9638e77e780d24aa0f54145ec72e933e
alattalatta <urty5656@gmail.com>
2019-02-25 23:43:19 +0900

    (chore) now.sh deploy test

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ const withTypescript = require('@zeit/next-typescript');
 const ForkTsCheckerWebapckPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = withTypescript({
-  target: 'server',
-  webpack(config, { isServer }) {
-    if (isServer) {
+  target: 'serverless',
+  webpack(config, { dev, isServer }) {
+    if (dev && isServer) {
       config.plugins.push(new ForkTsCheckerWebapckPlugin());
     }
     return config;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Link from "next/link";
 
 const Index: React.FunctionComponent = () => {
   return (
     <main>
       <h1>Hello!</h1>
+      <Link href="/about"><a>Go About</a></Link>
     </main>
   );
 };
