9944cfe05ac694e43d6837d960f67ca130177d58
alattalatta <urty5656@gmail.com>
2019-03-02 01:25:10 +0900

    (feat) Update packages, add CSS capability

--- next.config.js
+++ next.config.js
@@ -1,13 +1,21 @@
+const withCSS = require('@zeit/next-css');
 const withTypescript = require('@zeit/next-typescript');
 const ForkTsCheckerWebapckPlugin = require('fork-ts-checker-webpack-plugin');
 
-module.exports = withTypescript({
-  target: 'serverless',
-  webpack(config, { dev, isServer }) {
-    config.devtool = 'eval-source-map';
-    if (dev && isServer) {
-      config.plugins.push(new ForkTsCheckerWebapckPlugin());
-    }
-    return config;
-  },
-});
+module.exports = withTypescript(
+  withCSS({
+    cssModules: true,
+    cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: '[local][emoji][hash:base64:4]'
+    },
+    target: 'serverless',
+    webpack(config, { dev, isServer }) {
+      config.devtool = 'eval-source-map';
+      if (dev && isServer) {
+        config.plugins.push(new ForkTsCheckerWebapckPlugin());
+      }
+      return config;
+    },
+  })
+);
