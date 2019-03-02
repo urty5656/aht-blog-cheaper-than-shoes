aee6f07c3f612718e2da06db54c093a5ed616ee6
alattalatta <urty5656@gmail.com>
2019-03-03 03:42:06 +0900

    (feat) PostCSS nesting, cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   plugins: [
-    require('postcss-cssnext')
-  ]
+    require('postcss-import')(),
+    require('postcss-preset-env')({
+      features: {
+        'custom-media-queries': true,
+        'nesting-rules': true,
+      },
+    }),
+    require('cssnano')({
+      preset: [
+        'default',
+        {
+          reduceIdents: true,
+        },
+      ],
+    }),
+  ],
 };
